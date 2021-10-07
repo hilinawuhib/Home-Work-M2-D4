246d50e9338899ac0aa4934d01b60543b0dd4e55
hilina88 <hilinawuhib@gmail.com>
2021-10-07 16:50:40 +0200

    adding names to list

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
-
+function addNamesToList(){
+    let names = [];
+    document.getElementById("btnid").addEventListener('click', function addNamesToList () {
+        names.push(document.getElementById("name").value);
+        document.getElementById('groupd-id').innerHTML = names.join(' ');
+    });
+}
+addNamesToList()
